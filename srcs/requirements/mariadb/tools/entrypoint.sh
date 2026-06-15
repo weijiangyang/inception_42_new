5349e8e3446f5c02e687a7c91f6ba38b3e46b2f6
weiyang <weiyang@f4r5s16.paris.42.school>
2026-06-15 18:17:46 +0200

    commit

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -11,8 +11,6 @@
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/sh
-
 set -Eeuo pipefail
 
 echo "[MariaDB] Starting entrypoint..."
